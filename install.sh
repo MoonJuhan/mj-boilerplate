b8b219b3cc57e34134be050f4379431110012000
MoonJuhan <codemike@naver.com>
2021-10-21 18:30:56 +0900

    feat: 프로젝트 이름 읽기 스크립트 작성

--- install.sh
+++ install.sh
@@ -1,1 +1,30 @@
-echo "hello, Im Shell Script"
+echo "create-mj-boilerplate"
+
+name=""
+
+function setName() {
+    printf "Enter App Name: "
+    read name
+    echo "App Name is $name"
+
+    if [ ! -d $name ]; then
+        mkdir $name
+    else
+        echo "$name is exist"
+    fi
+}
+
+setName
+
+# mkdir $name
+
+# SELECT=""
+
+# while [[ "$SELECT" != $'\x0a' && "$SELECT" != $'\x20' ]]; do
+#     echo "Press <Space> to move selection"
+#     echo "Press <Enter> to confirm selection"
+#     read -d'' -s -n1
+#     echo "Debug/$SELECT/${#SELECT}"
+#     [[ "$SELECT" == $'\x0a' ]] && echo "enter" # do your install stuff
+#     [[ "$SELECT" == $'\x20' ]] && echo "space" && echo -ne "$options" # reprint options
+# done
