6413f14fc3dc08f6ab22b2ab04b6f71c3cec22d5
MoonJuhan <codemike@naver.com>
2021-10-22 18:29:22 +0900

    init: vue3 프로젝트 초기 구조 생성 진행중

--- vue3/src/main.js
+++ vue3/src/main.js
@@ -1,5 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+import router from './router'
+import store from './store'
+import plugins from './plugins'
 
-createApp(App).mount('#app')
+const boot = async () => {
+  const app = createApp(App)
+  app.use(store).use(router)
+
+  Object.values(plugins).forEach(app.use)
+
+  //   const result = await import('./global-components')
+  //   result.default(app)
+
+  app.mount('#app')
+}
+
+boot()
